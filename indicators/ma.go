36beb7f435a97b635fd37afc20c01bd168b9dcb0
Franco Valencia <franco.avalencia@gmail.com>
2021-06-22 14:09:00 -0500

    Added template for MA indicators

---
+++ indicators/ma.go
@@ -0,0 +1,10 @@
+package indicators
+
+import "github.com/vanclief/finmod/market"
+
+func MovingAverage(candles []market.Candle, length int) ([]float32, error) {
+
+	// Take from candles the last X where X = length
+
+	return nil, nil
+}
