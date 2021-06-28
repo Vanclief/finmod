3e44306230c595ad330199ed1e7af9c390289129
Franco Valencia <franco.avalencia@gmail.com>
2021-06-28 12:07:28 -0500

    Added balances

---
+++ market/balances.go
@@ -0,0 +1,13 @@
+package market
+
+// BalanceSnapshot - An snapshot of the total amount of assets
+type BalanceSnapshot struct {
+	Time     float64
+	Balances []Balance `json:"balances"`
+}
+
+// Balance - An asset and its unitary amount
+type Balance struct {
+	Asset  *Asset  `json:"asset"`
+	Amount float64 `json:"amount"`
+}
