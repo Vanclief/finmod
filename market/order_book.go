f6f67f7a18cf3dea5baa50dee591765fc1a98533
Franco Valencia <franco.avalencia@gmail.com>
2021-06-29 11:23:31 -0500

    Small tweeks to Orderbook GetDepth function

--- market/order_book.go
+++ market/order_book.go
@@ -1,8 +1,9 @@
 package market
 
 import (
-	"github.com/vanclief/ez"
 	"math"
+
+	"github.com/vanclief/ez"
 )
 
 // OrderBook - A record of active buy and sell orders in a single market
@@ -19,17 +20,22 @@ type OrderBookRow struct {
 	TotalVolume float64 `json:"total_volume"` // maybe should be called AccumulatedVolume
 }
 
-// ReturnVolumeOfOrderBook gives the accumulated volume from a price onwards,
-// it return -1 if it does not find an index for the selected price
-func (ob *OrderBook) ReturnVolumeOfOrderBook(price float64) (float64, error) {
-	op := "order_book.ReturnVolumeOfOrderBook"
-	firstAsk, lastAsk := ob.Asks[0], ob.Asks[len(ob.Asks) - 1]
-	firstBid, lastBid := ob.Bids[0], ob.Bids[len(ob.Bids) - 1]
+// GetDepth - returns the accumulated volume from a determined price
+// a price onwards
+func (ob *OrderBook) GetDepth(price float64) (float64, error) {
+	op := "OrderBook.GetDepth"
+
+	if price <= 0 {
+		return 0, ez.New(op, ez.EINVALID, "Price can't be equal or bellow zero", nil)
+	}
+
+	firstAsk, lastAsk := ob.Asks[0], ob.Asks[len(ob.Asks)-1]
+	firstBid, lastBid := ob.Bids[0], ob.Bids[len(ob.Bids)-1]
 	if firstAsk.Price <= price && price <= lastAsk.Price {
 		// price is in asks interval
 		for k := range ob.Asks {
 			if ob.Asks[k].Price > price {
-				index := math.Max(0, float64(k - 1))
+				index := math.Max(0, float64(k-1))
 				return ob.Asks[int(index)].Price, nil
 			}
 		}
@@ -38,11 +44,11 @@ func (ob *OrderBook) ReturnVolumeOfOrderBook(price float64) (float64, error) {
 		// price is in bids interval
 		for k := range ob.Bids {
 			if ob.Bids[k].Price < price {
-				index := math.Max(0, float64(k - 1))
+				index := math.Max(0, float64(k-1))
 				return ob.Bids[int(index)].Price, nil
 			}
 		}
 		return lastBid.Price, nil
 	}
-	return -1, ez.New(op, ez.ENOTFOUND, "index for price not found", nil)
+	return 0, ez.New(op, ez.ENOTFOUND, "No depth for the selected price", nil)
 }
--- market/order_book_test.go
+++ market/order_book_test.go
@@ -1,140 +1,144 @@
 package market
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
-  "time"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vanclief/ez"
 )
 
-func TestOrderBook_ReturnVolumeOfOrderBook(t *testing.T) {
-  sampleOrderBook := ReturnOrderBook()
+func TestOrderBookGetDepth(t *testing.T) {
+	sampleOrderBook := getTestOrderBook()
 
-  index, err := sampleOrderBook.ReturnVolumeOfOrderBook(5)
-  assert.Nil(t, err)
-  assert.Equal(t, float64(5), index)
-  index, err = sampleOrderBook.ReturnVolumeOfOrderBook(11)
-  assert.Nil(t, err)
-  assert.Equal(t, float64(11), index)
-  index, err = sampleOrderBook.ReturnVolumeOfOrderBook(14.33)
-  assert.Nil(t, err)
-  assert.Equal(t, float64(15), index)
-  index, err = sampleOrderBook.ReturnVolumeOfOrderBook(100)
-  assert.NotNil(t, err)
-  assert.Equal(t, float64(-1), index)
-  index, err = sampleOrderBook.ReturnVolumeOfOrderBook(-2)
-  assert.NotNil(t, err)
-  assert.Equal(t, float64(-1), index)
-  index, err = sampleOrderBook.ReturnVolumeOfOrderBook(9)
-  assert.Nil(t, err)
-  assert.Equal(t, float64(9), index)
+	index, err := sampleOrderBook.GetDepth(5)
+	assert.Nil(t, err)
+	assert.Equal(t, float64(5), index)
+	index, err = sampleOrderBook.GetDepth(11)
+	assert.Nil(t, err)
+	assert.Equal(t, float64(11), index)
+	index, err = sampleOrderBook.GetDepth(14.33)
+	assert.Nil(t, err)
+	assert.Equal(t, float64(15), index)
+	index, err = sampleOrderBook.GetDepth(100)
+	assert.NotNil(t, err)
+	assert.Equal(t, ez.ENOTFOUND, ez.ErrorCode(err))
+	assert.Equal(t, float64(0), index)
+	index, err = sampleOrderBook.GetDepth(-2)
+	assert.NotNil(t, err)
+	assert.Equal(t, ez.EINVALID, ez.ErrorCode(err))
+	assert.Equal(t, float64(0), index)
+	index, err = sampleOrderBook.GetDepth(9)
+	assert.Nil(t, err)
+	assert.Equal(t, float64(9), index)
 }
 
-func ReturnOrderBook() OrderBook {
-  return OrderBook {
-    Time: float64(time.Now().Unix()),
-    Asks: []OrderBookRow{
-      {
-        Price: 0,
-        Volume: 1,
-        TotalVolume: 1,
-      },
-      {
-        Price: 1,
-        Volume: 1,
-        TotalVolume: 2,
-      },
-      {
-        Price: 2,
-        Volume: 1,
-        TotalVolume: 3,
-      },
-      {
-        Price: 3,
-        Volume: 1,
-        TotalVolume: 4,
-      },
-      {
-        Price: 4,
-        Volume: 1,
-        TotalVolume: 5,
-      },
-      {
-        Price: 5,
-        Volume: 1,
-        TotalVolume: 6,
-      },
-      {
-        Price: 6,
-        Volume: 1,
-        TotalVolume: 7,
-      },
-      {
-        Price: 7,
-        Volume: 1,
-        TotalVolume: 8,
-      },
-      {
-        Price: 8,
-        Volume: 1,
-        TotalVolume: 9,
-      },
-      {
-        Price: 9,
-        Volume: 1,
-        TotalVolume: 10,
-      },
-    },
-    Bids: []OrderBookRow {
-      {
-        Price: 20,
-        Volume: 1,
-        TotalVolume: 1,
-      },
-      {
-        Price: 19,
-        Volume: 1,
-        TotalVolume: 2,
-      },
-      {
-        Price: 18,
-        Volume: 1,
-        TotalVolume: 3,
-      },
-      {
-        Price: 17,
-        Volume: 1,
-        TotalVolume: 4,
-      },
-      {
-        Price: 16,
-        Volume: 1,
-        TotalVolume: 5,
-      },
-      {
-        Price: 15,
-        Volume: 1,
-        TotalVolume: 6,
-      },
-      {
-        Price: 14,
-        Volume: 1,
-        TotalVolume: 7,
-      },
-      {
-        Price: 13,
-        Volume: 1,
-        TotalVolume: 8,
-      },
-      {
-        Price: 12,
-        Volume: 1,
-        TotalVolume: 9,
-      },
-      {
-        Price: 11,
-        Volume: 1,
-        TotalVolume: 10,
-      },
-    },
-  }
+func getTestOrderBook() OrderBook {
+	return OrderBook{
+		Time: float64(time.Now().Unix()),
+		Asks: []OrderBookRow{
+			{
+				Price:       0,
+				Volume:      1,
+				TotalVolume: 1,
+			},
+			{
+				Price:       1,
+				Volume:      1,
+				TotalVolume: 2,
+			},
+			{
+				Price:       2,
+				Volume:      1,
+				TotalVolume: 3,
+			},
+			{
+				Price:       3,
+				Volume:      1,
+				TotalVolume: 4,
+			},
+			{
+				Price:       4,
+				Volume:      1,
+				TotalVolume: 5,
+			},
+			{
+				Price:       5,
+				Volume:      1,
+				TotalVolume: 6,
+			},
+			{
+				Price:       6,
+				Volume:      1,
+				TotalVolume: 7,
+			},
+			{
+				Price:       7,
+				Volume:      1,
+				TotalVolume: 8,
+			},
+			{
+				Price:       8,
+				Volume:      1,
+				TotalVolume: 9,
+			},
+			{
+				Price:       9,
+				Volume:      1,
+				TotalVolume: 10,
+			},
+		},
+		Bids: []OrderBookRow{
+			{
+				Price:       20,
+				Volume:      1,
+				TotalVolume: 1,
+			},
+			{
+				Price:       19,
+				Volume:      1,
+				TotalVolume: 2,
+			},
+			{
+				Price:       18,
+				Volume:      1,
+				TotalVolume: 3,
+			},
+			{
+				Price:       17,
+				Volume:      1,
+				TotalVolume: 4,
+			},
+			{
+				Price:       16,
+				Volume:      1,
+				TotalVolume: 5,
+			},
+			{
+				Price:       15,
+				Volume:      1,
+				TotalVolume: 6,
+			},
+			{
+				Price:       14,
+				Volume:      1,
+				TotalVolume: 7,
+			},
+			{
+				Price:       13,
+				Volume:      1,
+				TotalVolume: 8,
+			},
+			{
+				Price:       12,
+				Volume:      1,
+				TotalVolume: 9,
+			},
+			{
+				Price:       11,
+				Volume:      1,
+				TotalVolume: 10,
+			},
+		},
+	}
 }
