d8970942c1d13584aa57f533cc7f6ddfabcdd9fc
Franco Valencia <franco.avalencia@gmail.com>
2021-07-01 11:35:12 -0500

    Added OrderRequest method CalculateFields

--- market/order.go
+++ market/order.go
@@ -3,6 +3,8 @@ package market
 import (
 	"fmt"
 	"time"
+
+	"github.com/vanclief/ez"
 )
 
 // ActionType determines if the action is to buy or to sell
@@ -56,20 +58,83 @@ type Order struct {
 	Trades         []Trade
 }
 
-// OrderRequest - Request to create an order
-type OrderRequest struct {
-	Action   ActionType
-	Type     OrderType
-	Pair     Pair
-	Price    float64
-	Quantity float64
-	Cost     float64
-}
-
 func (o *Order) String() string {
 	return fmt.Sprintf("ID: %s Action: %s Type: %s Volume: %.4f Cost: $%.4f", o.ID, o.Action, o.Type, o.Volume, o.Cost)
 }
 
+// OrderRequest - Request to create an order
+type OrderRequest struct {
+	Action   ActionType
+	Type     OrderType
+	Pair     *Pair
+	Price    float64
+	Quantity float64
+	Total    float64
+}
+
+// NewOrderRequest - creates a valid new OrderRequest
+func NewOrderRequest(pair *Pair, action ActionType, orderType OrderType, quantity, price, total float64) (*OrderRequest, error) {
+	const op = "market.NewOrderRequest"
+
+	if pair == nil {
+		return nil, ez.New(op, ez.EINVALID, "Order must have a pair defined", nil)
+	}
+
+	if action == "" || orderType == "" {
+		return nil, ez.New(op, ez.EINVALID, "Order must have an action and a type defined", nil)
+	}
+
+	if orderType == LimitOrder && price == 0 {
+		return nil, ez.New(op, ez.EINVALID, "LimitOrders must have a priced defined", nil)
+	}
+
+	if quantity == 0 && total == 0 {
+		return nil, ez.New(op, ez.EINVALID, "Total and quantity can't be both zero", nil)
+	}
+
+	request := &OrderRequest{
+		Action:   action,
+		Type:     orderType,
+		Pair:     pair,
+		Price:    price,
+		Quantity: quantity,
+		Total:    total,
+	}
+
+	return request, nil
+}
+
 func (o *OrderRequest) String() string {
-	return fmt.Sprintf("Action: %s Type: %s Quantity: %.4f Cost: $%.4f", o.Action, o.Type, o.Quantity, o.Cost)
+	return fmt.Sprintf("Action: %s | Type: %s | Pair: %s | Quantity: %.4f | Price: $%.4f | Total: $%.4f", o.Action, o.Type, o.Pair.String(), o.Quantity, o.Price, o.Total)
+}
+
+func (o *OrderRequest) CalculateFields(currentPrice float64) (*OrderRequest, error) {
+	const op = "OrderRequest.CalculateFields"
+
+	// Either the total cost or the quantity must be defined
+	if o.Total == 0 && o.Quantity == 0 {
+		return nil, ez.New(op, ez.EINVALID, "Total and quantity can't be both zero", nil)
+	} else if o.Total != 0 && o.Quantity != 0 {
+		return nil, ez.New(op, ez.EINVALID, "Either total or quantity must be zero", nil)
+	}
+
+	copy := *o
+
+	// MarketOrder
+	if o.Type == MarketOrder {
+		if o.Quantity != 0 {
+			copy.Total = o.Quantity * currentPrice
+		} else {
+			copy.Quantity = o.Total / currentPrice
+		}
+	} else {
+		if o.Quantity != 0 {
+			copy.Total = o.Quantity * o.Price
+		} else {
+			copy.Quantity = o.Total / o.Price
+		}
+	}
+
+	return &copy, nil
+
 }
---
+++ market/order_test.go
@@ -0,0 +1,62 @@
+package market
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOrderRequestCalculateFields(t *testing.T) {
+	type TestCase struct {
+		Action           ActionType
+		OrderType        OrderType
+		Quantity         float64
+		Price            float64
+		Total            float64
+		CurrentPrice     float64
+		ExpectedError    bool
+		ExpectedQuantity float64
+		ExpectedTotal    float64
+	}
+
+	baseAsset, _ := NewCryptoAsset("ETH")
+	quoteAsset, _ := NewForexAsset("USD")
+	pair, _ := NewPair(baseAsset, quoteAsset)
+
+	or, err := NewOrderRequest(pair, BuyAction, LimitOrder, 5, 2000, 0)
+	assert.Nil(t, err)
+
+	// Case 1: Should not overwrite the original order request
+	newOR, err := or.CalculateFields(0)
+	assert.Nil(t, err)
+	assert.NotEqual(t, or, newOR)
+
+	// Case 2: Should work
+
+	testCases := []TestCase{
+		// Action   OrderType    Q  P  T   CP     EE    EQ  ET
+		{BuyAction, MarketOrder, 10, 0, 0, 2000, false, 10, 20000},
+		{BuyAction, MarketOrder, 0, 0, 10000, 2000, false, 5, 10000},
+		{BuyAction, MarketOrder, 10, 10000, 0, 2000, false, 10, 20000},
+		{BuyAction, LimitOrder, 10, 2000, 0, 0, false, 10, 20000},
+		{BuyAction, LimitOrder, 0, 2000, 10000, 2000, false, 5, 10000},
+		{SellAction, MarketOrder, 10, 0, 0, 2000, false, 10, 20000},
+		{SellAction, MarketOrder, 0, 0, 10000, 2000, false, 5, 10000},
+		{SellAction, MarketOrder, 10, 10000, 0, 2000, false, 10, 20000},
+		{SellAction, LimitOrder, 10, 2000, 0, 0, false, 10, 20000},
+		{SellAction, LimitOrder, 0, 2000, 10000, 2000, false, 5, 10000},
+	}
+
+	for _, testCase := range testCases {
+		or, err = NewOrderRequest(pair, testCase.Action, testCase.OrderType, testCase.Quantity, testCase.Price, testCase.Total)
+		assert.Nil(t, err)
+		result, err := or.CalculateFields(testCase.CurrentPrice)
+		if testCase.ExpectedError {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.ExpectedTotal, result.Total)
+			assert.Equal(t, testCase.ExpectedQuantity, result.Quantity)
+		}
+	}
+}
