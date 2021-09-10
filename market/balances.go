7050a759ef8dc0b609e3369251188ad8d11fe38d
Franco Valencia <franco.avalencia@gmail.com>
2021-09-10 18:17:09 -0500

    Updated BalanceSnapshot and created AssetsSnapshot

--- market/balances.go
+++ market/balances.go
@@ -1,9 +1,17 @@
 package market
 
-// BalanceSnapshot - An snapshot of the total amount of assets
+// BalanceSnapshot - An snapshot of the current balance of a user
 type BalanceSnapshot struct {
-	Time     float64   `json:"time"`
-	Balances []Balance `json:"balances"`
+	Balance    float64 `json:"balance"`
+	Equity     float64 `json:"equity"`
+	Margin     float64 `json:"margin"`
+	FreeMargin float64 `json:"free_margin"`
+}
+
+// AssetsSnapshot - An snapshot of the total amount of assets
+type AssetsSnashot struct {
+	Time   float64   `json:"time"`
+	Assets []Balance `json:"assets"`
 }
 
 // Balance - An asset and its unitary amount
