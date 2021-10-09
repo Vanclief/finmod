10363c98c1ad091c6ed69be56062424911794d08
Franco Valencia <franco.avalencia@gmail.com>
2021-10-09 09:42:35 -0500

    Added PositionID to order request

--- market/order_request.go
+++ market/order_request.go
@@ -8,12 +8,13 @@ import (
 
 // OrderRequest - Request to create an order
 type OrderRequest struct {
-	Action   ActionType
-	Type     OrderType
-	Pair     *Pair
-	Price    float64
-	Quantity float64
-	Total    float64
+	Action     ActionType
+	Type       OrderType
+	Pair       *Pair
+	Price      float64
+	Quantity   float64
+	Total      float64
+	PositionID string
 }
 
 // NewOrderRequest - creates a valid new OrderRequest
