035764e0068cb1d2463f6f0f868cf7ff97ba574b
Luis Arturo Noria <lnoria86@outlook.com>
2022-02-09 19:05:17 -0600

    implemented williams fractal for odd number of candles

--- indicators/williams_fractals.go
+++ indicators/williams_fractals.go
@@ -3,151 +3,103 @@ package indicators
 import (
 	"fmt"
 	"github.com/vanclief/finmod/market"
-	"math"
+)
+
+type FractalType string
+
+var (
+	FractalUp   = FractalType("up")
+	FractalDown = FractalType("down")
 )
 
 type WilliamFractal struct {
-	Time        int64
-	Price       float64
-	FractalType int // 5 para la de 5 candles, 6 para la de 6 y así
+	Time          int64
+	Price         float64
+	Type          FractalType
+	Candles       []market.Candle
+	FractalLength int // 5 para la de 5 candles, 6 para la de 6 y así
 }
 
 func (wf *WilliamFractal) String() string {
-	return fmt.Sprintf("%d %f %d", wf.Time, wf.Price, wf.FractalType)
-}
-
-// Taken from https://forexsb.com/wiki/_media/indicators/fractal-types.png
-
-func HasRamp(candles []market.Candle) bool {
-	firstThree := candles[:3]
-	lastThree := candles[len(candles)-3:]
-	return math.Abs(firstThree[0].Close) < math.Abs(firstThree[1].Close) && math.Abs(firstThree[1].Close) < math.Abs(firstThree[2].Close) &&
-		math.Abs(lastThree[0].Close) > math.Abs(lastThree[1].Close) && math.Abs(lastThree[1].Close) > math.Abs(lastThree[2].Close)
-}
-
-func CandleDifferenceIsWithinDifferencePercentage(candle1, candle2 market.Candle, highLow string) bool {
-	percentage := 5 / 100.0
-	if highLow == "high" {
-		return math.Abs(candle1.High-candle2.High)/candle1.High < percentage
-	} else {
-		return math.Abs(candle1.Low-candle2.Low)/candle1.Low < percentage
+	candlesStrings := ""
+	for i, candle := range wf.Candles {
+		candlesStrings += fmt.Sprintf("%v,%v\n", i, candle.High)
 	}
+	for i, candle := range wf.Candles {
+		candlesStrings += fmt.Sprintf("%v,%v\n", i, candle.Low)
+	}
+	return fmt.Sprintf("%d %f %v %d\n%v", wf.Time, wf.Price, wf.Type, wf.FractalLength, candlesStrings)
 }
 
-func Cleanup(dirtyFractals []WilliamFractal) (cleanedFractals []WilliamFractal) {
-	for _, v := range dirtyFractals {
-		if v.Time != 0 {
-			cleanedFractals = append(cleanedFractals, v)
+func AllCandlesHighsLowerThanTestCandle(candles []market.Candle, testCandle market.Candle) bool {
+	for _, candle := range candles {
+		if candle.Time == testCandle.Time {
+			continue
+		}
+		if candle.High > testCandle.High {
+			return false
 		}
 	}
-	return cleanedFractals
+	return true
 }
 
-func WilliamsFractalController(candles []market.Candle) (fractals []WilliamFractal) {
+func AllCandlesLowsHigherThanTestCandle(candles []market.Candle, testCandle market.Candle) bool {
+	for _, candle := range candles {
+		if candle.Time == testCandle.Time {
+			continue
+		}
+		if candle.Low < testCandle.Low {
+			return false
+		}
+	}
+	return true
+}
+
+func CandlesContainFractal(candles []market.Candle) *WilliamFractal {
 	if len(candles) < 5 {
+		return nil
+	}
+	if len(candles)%2 == 0 {
+		return nil
+	}
+	middleCandle := candles[len(candles)/2]
+	if AllCandlesHighsLowerThanTestCandle(candles, middleCandle) {
+		return &WilliamFractal{
+			Time:          middleCandle.Time,
+			Price:         middleCandle.High,
+			Type:          FractalUp,
+			Candles:       candles,
+			FractalLength: len(candles),
+		}
+	} else if AllCandlesLowsHigherThanTestCandle(candles, middleCandle) {
+		return &WilliamFractal{
+			Time:          middleCandle.Time,
+			Price:         middleCandle.Low,
+			Type:          FractalDown,
+			Candles:       candles,
+			FractalLength: len(candles),
+		}
+	}
+	return nil
+}
+
+func WilliamsFractalController(candles []market.Candle, gapSize int) (fractals []WilliamFractal) {
+	if len(candles) < gapSize {
 		return
 	}
 	i := 0
-	j := 5
+	j := gapSize
 	for {
 		if j >= len(candles) {
 			break
 		}
 		testSubset := candles[i:j]
-		fractals = append(fractals, WilliamsFractalLocator(testSubset))
+		fractal := CandlesContainFractal(testSubset)
+		if fractal != nil {
+			fractals = append(fractals, *fractal)
+		}
 		i++
 		j++
 	}
-	return Cleanup(fractals)
-}
-
-func WilliamsFractalLocator(candles []market.Candle) (foundFractal WilliamFractal) {
-	// All fractals require at least 5 candles
-	if len(candles) < 5 {
-		return foundFractal
-	}
-	if !HasRamp(candles) {
-		return foundFractal
-	}
-	discriminantNumberOfCandles := len(candles) - 4
-	switch discriminantNumberOfCandles {
-	case 1:
-		return WilliamFractal{
-			Time:        candles[2].Time,
-			Price:       candles[2].Close,
-			FractalType: 5,
-		}
-	case 2:
-		if CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[3], "high") {
-			return WilliamFractal{
-				Time:        candles[3].Time,
-				Price:       candles[3].Close,
-				FractalType: 5,
-			}
-		} else {
-			return foundFractal
-		}
-	case 3:
-		diff23 := CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[3], "high")
-		diff24 := CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[4], "high")
-		if diff24 {
-			if !diff23 {
-				return WilliamFractal{
-					Time:        candles[3].Time,
-					Price:       candles[3].Close,
-					FractalType: 5,
-				}
-			} else {
-				return WilliamFractal{
-					Time:        candles[4].Time,
-					Price:       candles[4].Close,
-					FractalType: 5,
-				}
-			}
-		} else {
-			return foundFractal
-		}
-	case 4:
-		diff23 := CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[3], "high")
-		diff25 := CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[5], "high")
-		diff34 := CandleDifferenceIsWithinDifferencePercentage(candles[3], candles[4], "low")
-		diff45 := CandleDifferenceIsWithinDifferencePercentage(candles[4], candles[5], "high")
-		if diff25 && diff34 {
-			if !diff23 && diff45 {
-				return WilliamFractal{
-					Time:        candles[4].Time,
-					Price:       candles[4].Close,
-					FractalType: 5,
-				}
-			} else if diff23 && !diff45 {
-				return WilliamFractal{
-					Time:        candles[3].Time,
-					Price:       candles[3].Close,
-					FractalType: 5,
-				}
-			} else {
-				return foundFractal
-			}
-		} else {
-			return foundFractal
-		}
-
-	case 5:
-		diff23 := CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[3], "high")
-		diff24 := CandleDifferenceIsWithinDifferencePercentage(candles[2], candles[4], "high")
-		diff46 := CandleDifferenceIsWithinDifferencePercentage(candles[4], candles[6], "high")
-		diff34 := CandleDifferenceIsWithinDifferencePercentage(candles[3], candles[4], "low")
-		diff45 := CandleDifferenceIsWithinDifferencePercentage(candles[4], candles[5], "low")
-		diff56 := CandleDifferenceIsWithinDifferencePercentage(candles[5], candles[6], "high")
-		if !diff23 && !diff34 && !diff45 && diff56 && diff24 && diff46 {
-			return WilliamFractal{
-				Time:        candles[3].Time,
-				Price:       candles[3].Close,
-				FractalType: 5,
-			}
-		} else {
-			return foundFractal
-		}
-	}
-	return foundFractal
+	return fractals
 }
--- indicators/williams_fractals_test.go
+++ indicators/williams_fractals_test.go
@@ -7,10 +7,15 @@ import (
 )
 
 func TestWilliamsFractal(t *testing.T) {
+	fractalSize := 5
 	candles, _, _, _, _, err := loadCandlesFromFile("./test_dataset/BINANCE_ETHUSD_60.csv")
 	assert.Nil(t, err)
-	ans := WilliamsFractalController(candles)
+	ans := WilliamsFractalController(candles, fractalSize)
 	for _, v := range ans {
-		fmt.Println(v.String())
+		if v.Type == "up" {
+			fmt.Printf("%v,%v\n", (v.Time-candles[0].Time)/3600, v.Price+3)
+		} else {
+			fmt.Printf("%v,%v\n", (v.Time-candles[0].Time)/3600, v.Price-3)
+		}
 	}
 }
