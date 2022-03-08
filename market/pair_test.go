afd069be07e1b420659b492167b42afb7058a3a6
Franco Valencia <franco.avalencia@gmail.com>
2022-03-08 08:47:51 -0600

    Added PairMappings

--- market/pair_test.go
+++ market/pair_test.go
@@ -1,6 +1,11 @@
 package market
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,4 +25,35 @@ func TestPairSymbol(t *testing.T) {
 	assert.Equal(t, "ETH/USD", pair.Symbol("/"))
 	assert.Equal(t, "#US30", pairAlt.Symbol(""))
 	assert.Equal(t, "#US30", pairAlt.Symbol("/"))
+}
+
+func TestCreateMapping(t *testing.T) {
+
+	resp, _ := http.Get("https://api.binance.com/api/v3/exchangeInfo")
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	type Symbol struct {
+		Symbol     string `json:"symbol"`
+		BaseAsset  string `json:"baseAsset"`
+		QuoteAsset string `json:"quoteAsset"`
+	}
+
+	type Response struct {
+		Symbols []Symbol `json:"symbols"`
+	}
+
+	result := Response{}
+
+	err = json.Unmarshal(body, &result)
+	assert.Nil(t, err)
+
+	for _, symbol := range result.Symbols {
+		mapping := fmt.Sprintf(`"%s": {Base: Asset{Symbol: "%s"}, Quote: Asset{Symbol: "%s"}},`,
+			symbol.Symbol, symbol.BaseAsset, symbol.QuoteAsset)
+		fmt.Println(mapping)
+	}
+
 }
